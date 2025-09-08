8d53731313f6c03a1dccfdfe1ec9706c368f0778
Jayden Larkins <st10255814@vcconnect.edu.za>
2025-09-08 11:00:09 +0200

    Implemented retrofit timeouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     //retrofit library for api implementation
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
--- app/src/main/java/com/example/rentwise/retrofit_instance/RetrofitInstance.kt
+++ app/src/main/java/com/example/rentwise/retrofit_instance/RetrofitInstance.kt
@@ -1,17 +1,32 @@
 package com.example.rentwise.retrofit_instance
 
 import com.example.rentwise.api_interface_auth.UserAuth
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "https://rentwiseapi.onrender.com"
 
+    private val logging = HttpLoggingInterceptor().apply{
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(logging)
+        .build()
+
     val instance: UserAuth by lazy {
-        val retrofit = Retrofit.Builder()
+        Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        retrofit.create(UserAuth::class.java)
+            .create(UserAuth::class.java)
     }
 }
