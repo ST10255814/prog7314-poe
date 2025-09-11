0cb7c6230bb476e0ac73dc84011e534f7f49b612
ST10255814 <ST10255814@vcconnect.edu.za>
2025-09-11 22:54:23 +0200

    Implemented the endpoint to fetch notifications

--- app/src/main/java/com/example/rentwise/api_interface/RentWiseApi.kt
+++ app/src/main/java/com/example/rentwise/api_interface/RentWiseApi.kt
@@ -5,6 +5,7 @@ import com.example.rentwise.data_classes.FavouriteListingsResponse
 import com.example.rentwise.data_classes.LoginRequest
 import com.example.rentwise.data_classes.LoginResponse
 import com.example.rentwise.data_classes.ListingResponse
+import com.example.rentwise.data_classes.NotificationResponse
 import com.example.rentwise.data_classes.RegisterRequest
 import com.example.rentwise.data_classes.RegisterResponse
 import com.example.rentwise.data_classes.UserSettingsResponse
@@ -33,6 +34,9 @@ interface RentWiseApi {
 
     @POST("/api/{userID}/{listingID}/favourite")
     fun favouriteListing(@Path("userID") userId: String, @Path("listingID") listingId: String): Call<FavouriteListingPostResponse>
+
+    @GET("/api/notifications")
+    fun getNotifications() : Call<List<NotificationResponse>>
 
     /**@DELETE("api/{id}/unfavourite")
     fun deleteFavouriteListing(@Path("id") listingId: String): Call<UnfavouriteListingResponse>**/
